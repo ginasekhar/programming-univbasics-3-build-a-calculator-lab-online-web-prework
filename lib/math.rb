1464693cb5a461d08b970ba7de9731da0168b164
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-16 19:34:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,28 @@
+def valid_numbers(num1, num2)
+  
+end
+
 def addition(num1, num2)
   sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  return diff
 end
 
 def division(num1, num2)
-
+  if num2 == 0 then
+    puts "Invalid second argument"
+  else 
+    quotient = num1/num2
+    return quotient
+  end #if
 end
 
 def multiplication(num1, num2)
-
+  
 end
 
 def modulo(num1, num2)
